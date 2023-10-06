658efae8e39065f64d72cc662bf1d9f2b5f9dbae
Quinten Krauth <quintenkrauth19@gmail.com>
2023-10-06 17:19:31 -0400

    small css changes

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class HelloWorldBean {
@@ -21,8 +21,33 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name: String) {
+    // let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+    // let headers = new HttpHeaders({
+    //   Authorization: basicAuthHeaderString
+    // })
+
     return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
     // console.log("Hello Bean Service");
     
   }
+
+  // createBasicAuthenticationHttpHeader() {
+  //   let username = 'qkrauth';
+  //   let password = 'dummy';
+  //   let basicAuthHeaderString = 'Basic ' + window.btoa(`${username}:${password}`);
+  //   return basicAuthHeaderString;
+  // }
+
+  // Access to XMLHttpRequest at
+  // 'http://localhost:8080/hello-world/path-variable/qkrauth'
+  // from origin 'http://localhost:4200' has been blocked by CORS policy:
+  // No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
+  // Access to XMLHttpRequest at
+  // 'http://localhost:8080/hello-world/path-variable/qkrauth'
+  // from origin 'http://localhost:4200' has been blocked by CORS policy:
+  // Response to preflight request doesn't pass access control check:
+  // No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
 }
